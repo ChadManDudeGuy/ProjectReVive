9685643b785a0d2137c2b6bd2888f969335efbc3
12BrownBananas <garrettrossthompson@outlook.com>
2017-03-14 14:02:21 -0400

    Add files via upload

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+	private bool select; 
+	private bool selectDown;
+	private bool selectUp;
+	private bool direct;
+	private bool directDown;
+	private bool directUp;
+	// Use this for initialization
+	void Start () {
+		select = false;
+		selectUp = false;
+		selectDown = false;
+		direct = false;
+		directDown = false;
+		directUp = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			if (!select) {
+				selectDown = true;
+			}
+			else {
+				selectDown = false;
+			}
+			select = true;
+			selectUp = false;
+		}
+		else {
+			if (select) {
+				selectUp = true;
+			}
+			else {
+				selectUp = false;
+			}
+			select = false;
+			selectDown = false;
+		}
+		if (Input.GetMouseButton(1)) {
+			if (!direct) {
+				directDown = true;
+			}
+			else {
+				directDown = false;
+			}
+			direct = true;
+			directUp = false;
+		}
+		else {
+			if (direct) {
+				directUp = true;
+			}
+			else {
+				directUp = false;
+			}
+			direct = false;
+			directDown = false;
+		}
+	}
+	
+	public bool getSelect() {
+		return select;
+	}
+	public bool getSelectDown() {
+		return selectDown;
+	}
+	public bool getSelectUp() {
+		return selectUp;
+	}
+	public bool getDirect() {
+		return direct;
+	}
+	public bool getDirectDown() {
+		return directDown;
+	}
+	public bool getDirectUp() {
+		return directUp;
+	}
+}
---
+++ Assets/Scripts/UnitAI.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitAI : MonoBehaviour {
+	public GameObject selRing;
+	public Material consideredShader;
+	public Material selectedShader;
+	
+	public Material unitShader;
+	public Material unitHoldingShader;
+	public float yOffset = .5f;
+	
+	public bool selected = false;
+	private bool considered = false; //Is this unit being considered for selection?
+	private GameObject myRing;
+	
+	private GameObject targetObject;
+	private bool isGettingObject = false;
+	public bool holdingObject = false;
+	
+	private int carryingNum;
+	
+	public int angularSpd = 120;
+	
+	void Start() {
+		myRing = Instantiate(selRing);
+		myRing.transform.localScale = new Vector3(.75f, 1f, .75f);
+		myRing.GetComponent<Renderer>().material = consideredShader;
+		myRing.SetActive(false);
+	}
+	
+	void Update() {
+		myRing.transform.position = new Vector3(transform.position.x, transform.position.y+yOffset, transform.position.z);
+		if (GetComponent<InputManager>().getSelectUp()) {
+			checkSelected(true);
+		}
+		if (GetComponent<InputManager>().getSelect() == false) {
+			if (considered) {
+				considered = false;
+				if (selected == false) {
+					myRing.SetActive(false);
+				}
+			}
+			
+		}
+		if (holdingObject) {
+			GetComponent<Renderer>().material = unitHoldingShader;
+			float xOffset = targetObject.GetComponent<AppleScript>().xOffset[carryingNum];
+			float zOffset = targetObject.GetComponent<AppleScript>().zOffset[carryingNum];
+			float tarX = targetObject.transform.position.x;
+			float tarY = targetObject.transform.position.y;
+			float tarZ = targetObject.transform.position.z;
+			Vector3 dest = new Vector3(tarX+xOffset, tarY, tarZ+zOffset);
+			GetComponent<NavMeshAgent>().SetDestination(dest);
+			GetComponent<NavMeshAgent>().angularSpeed = 0;
+		}
+		else {
+			GetComponent<Renderer>().material = unitShader;
+			GetComponent<NavMeshAgent>().angularSpeed = angularSpd;
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.name == "obj_selector") {
+			considered = false;
+			myRing.SetActive(false);
+		}
+		if (isGettingObject) {
+			if (other.gameObject == targetObject) {
+				isGettingObject = false;
+			}
+		}
+		
+	}
+    
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "obj_selector") {
+			considered = true;
+			myRing.SetActive(true);
+			setShader(0);
+		}
+		if (isGettingObject && !holdingObject) {
+			if (other.gameObject == targetObject) {
+				holdingObject = true;
+			}
+		}
+	}
+	void setShader(int shaderType) {
+		if (shaderType == 0) {
+			myRing.GetComponent<Renderer>().material = consideredShader;
+		}
+		if (shaderType == 1) {
+			myRing.GetComponent<Renderer>().material = selectedShader;
+		}
+	}
+	
+	void checkSelected(bool absCheck) {
+		//absCheck: true is absolute check (e.g. deselect if not selected)
+		//false is relative check (e.g. just don't select if not selected)
+		if (considered) {
+			selected = true;
+			myRing.SetActive(true);
+			setShader(1);
+		}
+		else if (absCheck) {
+			selected = false;
+			myRing.SetActive(false);
+		}
+	}
+	
+	public void setGettingObject(bool val) {
+		isGettingObject = val;
+	}
+	public void setTargetObject(GameObject obj) {
+		targetObject = obj;
+	}
+	
+	public void pathToPoint(Vector3 point) {
+		NavMeshAgent myNav = GetComponent<NavMeshAgent>();
+		myNav.SetDestination(point);
+	}
+	
+	public GameObject getTargetObject() {
+		return targetObject;
+	}
+	
+	public bool getGettingObject() {
+		return isGettingObject;
+	}
+	
+	public int getCarryingNum() {
+		return carryingNum;
+	}
+	public void setCarryingNum(int num) {
+		carryingNum = num;
+	}
+}
